f35427cf6f5620aa71152c35dcfdb43f23f88964
Martin Schreiber <martin@mschreiber.at>
2017-05-04 15:27:25 +0200

    Initial Checkin
    Change-Id: I11966652e4ff24b51e69d15c878723d7e48de8c8

---
+++ tests/at.mschreiber.plugin1.tests/src/at/mschreiber/plugin1/tests/DummyTest.java
@@ -0,0 +1,13 @@
+package at.mschreiber.plugin1.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DummyTest {
+
+	@Test
+	public void doATest() {
+		Assert.assertTrue(true);
+	}
+
+}
